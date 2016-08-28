7e5c54030999655d89082d72a671f5dfd288930d
apache10 <gagu12ec@gmail.com>
2016-08-28 13:19:37 +0530

    sign Up page UI is implemented

--- locus/app/src/main/java/com/justiceleague/locus/LoginActivity.java
+++ locus/app/src/main/java/com/justiceleague/locus/LoginActivity.java
@@ -18,4 +18,5 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
+
 }
--- locus/app/src/main/java/com/justiceleague/locus/SignUpActivity.java
+++ locus/app/src/main/java/com/justiceleague/locus/SignUpActivity.java
@@ -9,5 +9,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
