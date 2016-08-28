d30711beb9c141c0a4dceaaaa8549b7ba659b713
Devil29 <vishalraj48@gmail.com>
2016-08-28 11:33:23 +0530

    Login and splash added

---
+++ locus/app/src/main/java/com/justiceleague/locus/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.justiceleague.locus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+/**
+ * Created by vishal.kumar1 on 28/08/16.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
